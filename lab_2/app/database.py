2a1bd2aa64965374f1cfc66afdf87aaad37a4b8c
grumpycatyo-collab <linkolnmax24@gmail.com>
2024-10-27 17:04:30 +0200

    fix docker compose

--- lab_2/app/database.py
+++ lab_2/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/products_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/products_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
